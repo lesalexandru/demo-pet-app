83986f58ce423f7937ca34d20c133c2f6bc3fc63
Alex Les <alexandru.les@mediaan.com>
2023-12-03 21:53:33 +0200

    Updating Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.google.dagger.hilt.android")
 }
 
-val keyStorePropertiesFile = rootProject.file("keystore.properties")
+val keyStorePropertiesFile: File = rootProject.file("keystore.properties")
 val keyStoreProperties = Properties()
 keyStoreProperties.load(FileInputStream(keyStorePropertiesFile))
 
@@ -81,7 +81,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Gson
-    implementation("com.google.code.gson:gson:2.9.1")
+    implementation("com.google.code.gson:gson:2.10")
 
     // Gson Converter
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -94,7 +94,7 @@ dependencies {
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     // Compose
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
